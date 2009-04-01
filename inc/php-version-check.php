6ddb498d885154dfc75b88c1c9132aa1f5351d9d
icanlocalize <icanlocalize@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-01 15:15:24 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@106975 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/php-version-check.php
+++ inc/php-version-check.php
@@ -4,9 +4,21 @@
       
       function icl_php_version_warn(){
           echo '<div class="error"><ul><li><strong>';
-          echo __('It appears that your host isn\'t running PHP5. SitePress requires PHP5 in order to work correctly. We recommend that you contact your hosting company and request them to switch you to PHP5.', 'sitepress');
-          echo '</strong></li></ul></div>';
+          echo __('SitePress cannot be activated becaue your version of PHP is too old. To run correctly, you must have PHP5 installed. We recommend that you contact your hosting company and request them to switch you to PHP5.', 'sitepress');
+          echo '</strong></li></ul></div>';                    
       }
-  }
-  
+      
+      $active_plugins = get_option('active_plugins');
+      $icl_sitepress_idx = array_search('sitepress-multilingual-cms/sitepress.php', $active_plugins);
+      if(false !== $icl_sitepress_idx){
+          unset($active_plugins[$icl_sitepress_idx]);
+          update_option('active_plugins', $active_plugins);
+          unset($_GET['activate']);
+          $recently_activated = get_option('recently_activated');
+          if(!isset($recently_activated['sitepress-multilingual-cms/sitepress.php'])){
+              $recently_activated['sitepress-multilingual-cms/sitepress.php'] = time();
+              update_option('recently_activated', $recently_activated);
+          }
+      }      
+  }  
 ?>
