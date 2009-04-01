0042c161f6ba6ca34b060ef12604adca985927f6
icanlocalize <icanlocalize@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-01 20:43:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@107039 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/php-version-check.php
+++ inc/php-version-check.php
@@ -1,10 +1,10 @@
 <?php
-  if(version_compare(phpversion(), '5', '<')){
+  if(version_compare(phpversion(), '6', '<')){
       add_action('admin_notices', 'icl_php_version_warn');
       
       function icl_php_version_warn(){
           echo '<div class="error"><ul><li><strong>';
-          echo __('SitePress cannot be activated becaue your version of PHP is too old. To run correctly, you must have PHP5 installed. We recommend that you contact your hosting company and request them to switch you to PHP5.', 'sitepress');
+          echo __('SitePress cannot be activated because your version of PHP is too old. To run correctly, you must have PHP5 installed. We recommend that you contact your hosting company and request them to switch you to PHP5.', 'sitepress');
           echo '</strong></li></ul></div>';                    
       }
       
