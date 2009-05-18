e996c13a1a49bf9586b0c67a0b11ae863c94d364
icanlocalize <icanlocalize@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-18 13:22:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@118691 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menu/language-selector.php
+++ menu/language-selector.php
@@ -4,13 +4,7 @@
             <?php if(isset($main_language['country_flag_url'])):?>                
             <img class="iclflag" src="<?php echo $main_language['country_flag_url'] ?>" alt="<?php echo $main_language['language_code'] ?>" width="18" height="12" />                                
             &nbsp;<?php endif; ?>
-            <?php 
-                if(isset($main_language['native_name']) || (!isset($main_language['country_flag_url']) && !isset($main_language['translated_name']))){
-                    echo $main_language['native_name']; 
-                }elseif(isset($main_language['translated_name'])){
-                    echo $main_language['translated_name']; 
-                } 
-            ?>
+            <?php echo icl_disp_language($main_language['native_name'], $main_language['translated_name']) ?>
             <?php if(!isset($ie_ver) || $ie_ver > 6): ?></a><?php endif; ?>
             <?php if(isset($ie_ver) && $ie_ver <= 6): ?><table><tr><td><?php endif ?>
             <ul>
@@ -20,16 +14,7 @@
                     <?php if(isset($lang['country_flag_url'])):?>                
                     <img class="iclflag" src="<?php echo $lang['country_flag_url'] ?>" alt="<?php echo $lang['language_code'] ?>" width="18" height="12" />&nbsp;                    
                     <?php endif; ?>
-                    <?php 
-                        if(isset($lang['native_name'])){ 
-                            echo $lang['native_name'];
-                        }                
-                        if(isset($lang['translated_name']) && $lang['translated_name'] != $lang['native_name']){
-                            if(isset($lang['native_name'])) echo ' (';
-                            echo $lang['translated_name'];
-                            if(isset($lang['native_name'])) echo ')';
-                        }
-                    ?>
+                    <?php echo icl_disp_language($lang['native_name'], $lang['translated_name']) ?>
                     </a>
                 </li>
                 <?php endforeach; ?>
