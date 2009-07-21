8ad34b1778f951bf0088df12e94352436f6717ca
icanlocalize <icanlocalize@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-21 08:52:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@137335 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/plugins-texts-functions.php
+++ inc/plugins-texts-functions.php
@@ -84,7 +84,7 @@ function icl_pt_handle_upload(){
         }                
     }
     fclose($fh);            
-    if($file['error']==0 && $file['size'] && $file['type']=='text/csv' && !$uplerr){
+    if($file['error']==0 && $file['size'] && /*$file['type']=='text/csv' &&*/ !$uplerr){
         $wpdb->query("DELETE FROM {$wpdb->prefix}icl_plugins_texts WHERE plugin_name='{$plugin}'");
         $fh = fopen($file['tmp_name'], 'rb');
         while($data = fgetcsv($fh)){
