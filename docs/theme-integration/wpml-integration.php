4cf3edf5b0ab7febf5c463994fe9e63915e1415b
icanlocalize <icanlocalize@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-12 14:04:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@145205 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ docs/theme-integration/wpml-integration.php
@@ -0,0 +1,68 @@
+<?php 
+
+// HOME URL
+// USAGE: replace references to the blog home url such as:
+// - get_option('home')
+// - bloginfo('home')
+// - bloginfo('url')
+// - get_bloginfo('url')
+// - etc...
+// with icl_get_home_url()
+if(!function_exists('icl_get_home_url')){
+    function icl_get_home_url(){
+        get_bloginfo('url');
+    }
+}
+
+
+
+// LANGUAGE SELECTOR
+// USAGE place this on the single.php, page.php, index.php etc... - inside the loop
+// function icl_content_languages($args)
+// args: skip_missing, before, after
+// defaults: skip_missing = 1, before =  __('This post is also available in: '), after = ''
+function icl_content_languages($args=''){
+    parse_str($args);
+    if(function_exists('icl_get_languages')){
+        $languages = icl_get_languages($args);
+        if(1 < count($languages)){
+            echo isset($before) ? $before : __('This post is also available in: ');
+            foreach($languages as $l){
+                if(!$l['active']) $langs[] = '<a href="'.$l['url'].'">'.$l['translated_name'].'</a>';
+            }
+            echo join(', ', $langs);
+            echo isset($after) ? $after : '';
+        }    
+    }
+} 
+
+
+// LINKS TO SPECIFIC ELEMENTS
+// USAGE
+// args: $element_id, $element_type='post', $link_text='', $optional_parameters=array(), $anchor=''
+if(!function_exists('icl_link_to_element')){
+    function icl_link_to_element($element_id, $element_type='post', $link_text='', $optional_parameters=array(), $anchor=''){
+        switch($element_type){
+            case 'post':
+            case 'page':
+                $ret = '<a href="'.get_permalink($element_id).'">';
+                if($anchor){
+                    $ret .= $anchor;
+                }else{
+                    $ret .= get_the_title($element_id);
+                }
+                $ret .= '<a>'; 
+                break;
+            case 'tag':
+            case 'post_tag':
+                $tag = get_term_by('id', $element_id, 'tag', ARRAY_A);
+                $ret = '<a href="'.get_tag_link($element_id).'">' . $tag->name . '</a>';
+            case 'category':
+                $ret = '<a href="'.get_tag_link($element_id).'">' . get_the_category_by_ID($element_id) . '</a>';
+            default: $ret = '';           
+        }
+        return $ret;
+    }        
+}
+
+?>
