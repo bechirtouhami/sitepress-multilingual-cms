63e9500a20fe99bdd254362f64e5f2bb7ec78d86
brucepearson <brucepearson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-16 09:40:54 +0000

    change the refer if we came from an action=edit page.
    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@155265 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menu/category-menu.php
+++ menu/category-menu.php
@@ -1,4 +1,15 @@
 <?php $this->noscript_notice() ?>
+
+<?php
+    if ($referer = $_SERVER['HTTP_REFERER']) {
+        if (false !== strpos($referer, 'action=edit')) {
+            // we have come from another edit page and we don't want to
+            // return there after the "Update"
+            $ref = "/wp-admin/categories.php";
+            echo '<input type="hidden" name="_wp_original_http_referer" value="' . attribute_escape( stripslashes( $ref ) ) . '" />';
+        }
+    }
+?>
 <div id="icl_category_menu" style="display:none">
 
 <div id="dashboard-widgets" class="metabox-holder">
@@ -116,30 +127,6 @@ function get_category_name($id) {
     <?php endif; ?>
     
     <br clear="all" style="line-height:1px;" />
-<?php endif; ?>
-
-<?php if($trid && false): ?>
-    <p><?php echo __('Translations', 'sitepress') ?> (<a href="javascript:;" 
-        onclick="jQuery('#icl_translations_table').toggle();if(jQuery(this).html()=='<?php echo __('hide','sitepress')?>') jQuery(this).html('<?php echo __('show','sitepress')?>'); else jQuery(this).html('<?php echo __('hide','sitepress')?>')"><?php echo __('show','sitepress')?></a>)</p>
-    <table width="100%" id="icl_translations_table" style="display:none">
-    <th align="left"><?php echo __('Language', 'sitepress') ?></th>
-    <th align="left"><?php echo __('Name', 'sitepress') ?></th>
-    <th align="right"><?php echo __('Operations', 'sitepress') ?></th>
-    <?php foreach($active_languages as $lang): if($this_lang==$lang['code']) continue;?>
-    <tr>
-        <td><?php echo $lang['display_name'] ?></td>
-        <?php $edit_link = "categories.php?action=edit&amp;cat_ID=" . $translations[$lang['code']]->term_id; ?>
-        <td><?php echo isset($translations[$lang['code']]->name)?'<a href="'.$edit_link.'" title="'.__('Edit','sitepress').'">'.$translations[$lang['code']]->name.'</a>':__('n/a','sitepress') ?></td>
-        <td align="right">
-            <?php if(!isset($translations[$lang['code']]->element_id)):?>            
-            <a href="categories.php?trid=<?php echo $trid ?>&lang=<?php echo $lang['code'] ?>"><?php echo __('add','sitepress') ?></a>
-            <?php else: ?>            
-            <a href="<?php echo get_category_link($translations[$lang['code']]->term_id) ?>" target="_blank"><?php echo __('View','sitepress') ?></a>
-            <?php endif; ?>        
-        </td>
-    </tr>
-    <?php endforeach; ?>
-    </table>
 <?php endif; ?>
 
 </div>
