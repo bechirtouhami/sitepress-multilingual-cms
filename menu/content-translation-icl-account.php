3b6695c05f3d95f344f18c8782197109622e7813
brucepearson <brucepearson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-24 05:56:57 +0000

    fix default button for "Enter" key.
    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@157474 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menu/content-translation-icl-account.php
+++ menu/content-translation-icl-account.php
@@ -53,6 +53,9 @@
                                     <p class="submit">
                                         <a href="javascript:;" onclick="jQuery('#icl_create_account').hide();jQuery('#icl_configure_account').fadeIn();"><?php echo __('I already have an account at ICanLocalize', 'sitepress') ?></a>
                                         <div style="text-align:right">
+                                            <?php //Hidden button for catching "Enter" key ?>
+                                            <input id="icl_content_trans_setup_finish_enter" class="button-primary" name="icl_content_trans_setup_finish_enter" value="<?php echo __('Add project to my account and finish', 'sitepress') ?>" type="submit" style="display:none"/>
+
                                             <input class="button" name="icl_content_trans_setup_cancel" value="<?php echo __('Cancel', 'sitepress') ?>" type="button" />
                                             <input id="icl_content_trans_setup_back_2" class="button-primary" name="icl_content_trans_setup_back_2" value="<?php echo __('Back', 'sitepress') ?>" type="submit" />
                                             <input id="icl_content_trans_setup_finish" class="button-primary" name="icl_content_trans_setup_finish" value="<?php echo __('Create account and Finish', 'sitepress') ?>" type="submit" />
