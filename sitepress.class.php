3e414b3ef81106bf9cd3cea1fdf2da4c97abc65b
mihaimihai <mihaimihai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-28 21:59:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@158748 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sitepress.class.php
+++ sitepress.class.php
@@ -2689,12 +2689,12 @@ class SitePress{
             if($__management_columns_posts_translations[$id][$v['code']]){
                 $img = 'edit_translation.png';
                 $alt = sprintf(__('Edit the %s translation'), $v['display_name']);                
-                $link = 'post.php?action=edit&post='.$__management_columns_posts_translations[$id][$v['code']]->element_id.'&lang='.$v['code'];
+                $link = 'post.php?action=edit&amp;post='.$__management_columns_posts_translations[$id][$v['code']]->element_id.'&amp;lang='.$v['code'];
             }else{
                 $img = 'add_translation.png';
                 $alt = sprintf(__('Add translation to %s'), $v['display_name']);
                 $src_lang = $this->get_current_language() == 'all' ? $this->get_default_language() : $this->get_current_language();
-                $link = 'post-new.php?trid=' . $__management_columns_posts_translations[$id][$this->get_current_language()]->trid.'&lang='.$v['code'].'&source_lang=' . $src_lang;
+                $link = 'post-new.php?trid=' . $__management_columns_posts_translations[$id][$this->get_current_language()]->trid.'&amp;lang='.$v['code'].'&amp;source_lang=' . $src_lang;
             }
             echo '<a href="'.$link.'" title="'.$alt.'">';
             echo '<img src="'.ICL_PLUGIN_URL . '/res/img/' .$img.'" alt="'.$alt.'" width="16" height="16" />';
