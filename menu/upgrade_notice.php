8ddee56c33eb1e57a3fe940f718bfbe83971a2b1
icanlocalize <icanlocalize@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-30 16:26:32 +0000

    added notice for 1.3.1
    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@159413 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menu/upgrade_notice.php
+++ menu/upgrade_notice.php
@@ -1,6 +1,6 @@
 <?php 
 $upgrade_lines =  array(
-    '1.3.1' => __('stuff added in 1.3.1', 'sitepress')
+    '1.3.1' => __('translation controls on posts and pages lists', 'sitepress')
 );
 
 $short_v = implode('.', array_slice(explode('.', ICL_SITEPRESS_VERSION), 0, 3));
@@ -9,7 +9,7 @@ if(!isset($upgrade_lines[$short_v])) return;
 ?>
 <br clear="all" />
 <div id="message" class="updated message fade" style="clear:both;margin-top:5px;">
-    <p><?php printf(__('New in WPML %s: %s', 'sitepress'), $short_v, $upgrade_lines[$short_v]); ?></p>
+    <p><?php printf(__('New in WPML %s: <b>%s</b>', 'sitepress'), $short_v, $upgrade_lines[$short_v]); ?></p>
     <p>
         <a href="http://wpml.org/home/wpml-news/"><?php _e('Learn more', 'sitepress')?></a>&nbsp;|&nbsp;
         <a title="<?php _e('Stop showing this message', 'sitepress') ?>" id="icl_dismiss_upgrade_notice" href="#"><?php _e('Dismiss', 'sitepress') ?></a>
