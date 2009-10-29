18ea22c1fcd8347289d563db05f84996858e148f
brucepearson <brucepearson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-29 05:37:06 +0000

    complete deletion of pro-translation settings.
    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@168191 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/hacks/debug-actions.php
+++ inc/hacks/debug-actions.php
@@ -11,8 +11,21 @@ switch($_GET['debug_action']){
         unset($sitepress_settings['site_id']);
         unset($sitepress_settings['access_key']);
         unset($sitepress_settings['translator_choice']);
+        unset($sitepress_settings['icl_lang_status']);
+        unset($sitepress_settings['icl_balance']);
+        unset($sitepress_settings['icl_support_ticket_id']);
+        unset($sitepress_settings['icl_current_session']);
+        unset($sitepress_settings['last_get_translator_status_call']);
+        unset($sitepress_settings['last_icl_reminder_fetch']);
         
         update_option('icl_sitepress_settings', $sitepress_settings);
+        
+        mysql_query("TRUNCATE TABLE {$wpdb->prefix}icl_core_status");
+        mysql_query("TRUNCATE TABLE {$wpdb->prefix}icl_content_status");
+        mysql_query("TRUNCATE TABLE {$wpdb->prefix}icl_string_status");
+        mysql_query("TRUNCATE TABLE {$wpdb->prefix}icl_node");
+        mysql_query("TRUNCATE TABLE {$wpdb->prefix}icl_reminders");
+        
         header("Location: admin.php?page=".basename(ICL_PLUGIN_PATH).'/menu/content-translation.php');
         exit;
     
