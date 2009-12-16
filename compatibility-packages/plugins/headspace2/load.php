77c2fff738994eda8d9e83c6e0459da9ae02fbfa
mihaimihai <mihaimihai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-16 13:52:16 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@183450 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- compatibility-packages/plugins/headspace2/load.php
+++ compatibility-packages/plugins/headspace2/load.php
@@ -14,6 +14,27 @@ class WP_Headspace2_SEO_compatibility  extends WPML_Package{
     
     function __construct(){
         parent::__construct();
+        
+        if(!isset($this->settings['translation_sync_file_loaded']) || !$this->settings['translation_sync_file_loaded']){
+            global $wpdb;
+            $fh = fopen($this->package_path . '/res/hs_custom_fields.csv', 'rb');
+            while($data = fgetcsv($fh)){
+                $wpdb->insert($wpdb->prefix.'icl_plugins_texts', array(   
+                        'plugin_name'=>substr($data[0],0,128),
+                        'attribute_type' => substr($data[1], 0, 64),
+                        'attribute_name' => substr($data[2], 0, 128),
+                        'description'    => $data[3],
+                        'translate'      => $data[4]
+                    )
+                );
+            }
+            fclose($fh);
+            if($wpdb->get_var("SELECT COUNT(*) FROM {$wpdb->prefix}icl_plugins_texts WHERE plugin_name='".$this->data['Plugin']."'")){
+                $this->settings['translation_sync_file_loaded'] = true;
+            }            
+            
+        }
+        
     }
     
     function __destruct(){
