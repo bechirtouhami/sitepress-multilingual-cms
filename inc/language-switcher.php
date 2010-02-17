75ac076d9a1141adeb13f8f0d814c08c56a329ad
jozik <jozik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-17 09:44:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@206941 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/language-switcher.php
+++ inc/language-switcher.php
@@ -224,8 +224,7 @@ class SitePressLanguageSwitcher {
                     //echo icl_disp_language( $this->settings['icl_lso_native_lang'] ? $l['native_name'] : null, $this->settings['icl_lso_display_lang'] ? $l['translated_name'] : null );
                     //if(!$l['active']) echo '</a>';
 					echo '</a>';
-                    echo '</li>
-                    ';
+                    echo '</li>';
                 }
             echo '
                     </ul>
