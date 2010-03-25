33abf7a2559c8a1b6ccf8d94ff7f367d09112f60
mihaimihai <mihaimihai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-25 16:27:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@221657 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sitepress.php
+++ sitepress.php
@@ -5,7 +5,7 @@ Plugin URI: http://wpml.org/
 Description: WPML Multilingual CMS. <a href="http://wpml.org">Documentation</a>.
 Author: OnTheGoSystems
 Author URI: http://www.onthegosystems.com
-Version: 1.7.3
+Version: 1.7.3.1-dev
 */
 
 /*
@@ -27,7 +27,7 @@ Version: 1.7.3
 
 
 if(defined('ICL_SITEPRESS_VERSION')) return;
-define('ICL_SITEPRESS_VERSION', '1.7.3');
+define('ICL_SITEPRESS_VERSION', '1.7.3.1');
 define('ICL_PLUGIN_PATH', dirname(__FILE__));
 define('ICL_PLUGIN_FOLDER', basename(ICL_PLUGIN_PATH));
 
