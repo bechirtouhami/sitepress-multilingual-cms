a9aa32aa53ee5bf9edf387e76044af8029017a96
mihaimihai <mihaimihai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-08 20:24:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@250105 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menu/menu-footer.php
+++ menu/menu-footer.php
@@ -1,8 +1,5 @@
-<div style="border-top: 2pt solid #DFDFDF; margin:1em 0 1em 0; padding: 0 1em 0 1em;">
-<h3><?php echo __('Need help?', 'sitepress')?></h3>
-
-<p>
-<a href="http://wpml.org/?page_id=505"><?php _e('WPML user guide', 'sitepress')?></a> &nbsp; | &nbsp;
-<a href="http://wpml.org/?page_id=3884"><?php _e('Information for web developers', 'sitepress')?></a> &nbsp;
-</p>
+<div class="updated message">
+<h3 style="margin-bottom:0px;"><?php echo __('Need help?', 'sitepress')?></h3>
+<p style="line-height:1.4em;"><?php printf(__("Purchase a <a%s>support subscription</a> and get help directly from WPML's developers. We will help you get the most out of WPML and customize it for your site.", 'sitepress'), 
+    ' href="admin.php?page='.ICL_PLUGIN_FOLDER.'/menu/support.php"'); ?></p>
 </div>
