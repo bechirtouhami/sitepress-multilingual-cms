d5d1cdb67926061fa6c08927bb69ffeb933ab77f
icanlocalize <icanlocalize@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-01 09:54:00 +0000

    added upgrade notice for 1.8.1
    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@270245 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menu/upgrade_notice.php
+++ menu/upgrade_notice.php
@@ -16,7 +16,8 @@ $upgrade_lines =  array(
     '1.7.6' => __('Works with WordPress 3','sitepress'),
     '1.7.7' => __('Supports custom taxonomies and lots of bug fixes for tags and categories','sitepress'),
     '1.7.8' => __('Supports custom post types','sitepress'),
-    '1.8.0' => __('Supports multilingual menus','sitepress')
+    '1.8.0' => __('Supports multilingual menus','sitepress'),
+    '1.8.1' => __('Multilingual menus bug fixes and improved translation interface','sitepress')
 );
 
 $short_v = implode('.', array_slice(explode('.', ICL_SITEPRESS_VERSION), 0, 3));
