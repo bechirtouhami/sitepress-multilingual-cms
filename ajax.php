7b863cdfda584b5bcab307219c2074f2f698e65d
mihaimihai <mihaimihai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-06 12:41:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@272390 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ajax.php
+++ ajax.php
@@ -698,7 +698,6 @@ switch($_REQUEST['icl_ajx_action']){
                 }else{
                     $url_glue = '?accesskey='.$this->settings['access_key'] . '&compact=1';
                 }
-                $url_glue = false !== strpos($url,'?') ? '&' : '?';
                 $output .= $message . ' - ' . $this->create_icl_popup_link(ICL_API_ENDPOINT. $url . $url_glue . '&message_id=' . $r->id. '&TB_iframe=true') . __('View', 'sitepress') . '</a>';
 
                 if ($r->can_delete == '1') {
