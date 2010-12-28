13d8e40345e219928ee2440be1511bfe050838ba
mihaimihai <mihaimihai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-28 17:32:33 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sitepress-multilingual-cms/trunk@326733 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/icl_api.php
+++ lib/icl_api.php
@@ -266,6 +266,9 @@ class ICanLocalizeQuery{
     function cms_requests_all(){
         $request_url = ICL_API_ENDPOINT . '/websites/' . $this->site_id . '/cms_requests.xml?show_languages=1&accesskey=' . $this->access_key;        
         $res = $this->_request($request_url);
+        
+        if($res === false) return false;
+        
         if(empty($res['info']['pending_cms_requests']['cms_request'])){
             $pending_requests = array();
         }elseif(count($res['info']['pending_cms_requests']['cms_request'])==1){
